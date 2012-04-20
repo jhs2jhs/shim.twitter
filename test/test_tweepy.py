9fb82ca9b5ff85b1acb4d84e1f0cc584e262e1a0
jianhuashao <jianhua.shao1986@gmail.com>
2012-04-20 20:01:51 +0100

    check the network proxy in running environment

--- test/test_tweepy.py
+++ test/test_tweepy.py
@@ -5,3 +5,8 @@ print "tweepy is imported"
 public_tweets = tweepy.api.public_timeline()
 for tweet in public_tweets:
     print tweet.text
+
+
+import urllib
+proxies = urllib.getproxies()
+print "proxy setting: %s"%str(proxies)
