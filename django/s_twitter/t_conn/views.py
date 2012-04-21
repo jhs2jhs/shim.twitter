d1787f0dae9fa47118bbf9e19766e3fb1b28844c
jianhuashao <jianhua.shao1986@gmail.com>
2012-04-21 11:15:10 +0100

    prepare to import python-oauth module

--- django/s_twitter/t_conn/views.py
+++ django/s_twitter/t_conn/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import HttpResponse
+import json
 
 def hello(request):
     return HttpResponse('hello t_conn test')
@@ -9,8 +10,15 @@ def test_tweepy(request):
     public_tweets = tweepy.api.public_timeline()
     txt = ''
     for tweet in public_tweets:
-        print tweet.text
+        #print json.dumps(tweet)
         txt += tweet.text + '<br>'
-    print txt
+    #print txt
+    print tweepy.api.__getattribute__
+    print '\n'
+    print dir(public_tweets[0])
     return HttpResponse(txt)
         
+def convert_timeline_json(timeline):
+    j = []
+    for tl in timeline:
+        entitie = tl.entities
